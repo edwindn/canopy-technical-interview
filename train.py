dcdcbb0c14c404f989c6b0c57837f3c858e6e55d
edwincanopy <edn9@pm.me>
2025-04-25 17:36:35 -0700

    m

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ stream = load_dataset(
 )
 
 # 2. take however many examples you actually need, e.g. 100
-dataset = itertools.islice(stream, 100)
+dataset = itertools.islice(stream, 100000)
 # Convert iterator to list and create Dataset
 dataset = list(dataset)
 dataset = Dataset.from_list(dataset)
